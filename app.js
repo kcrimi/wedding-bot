69b6a78b2ae9983c92d282062db845baf174ccbe
kcrimi <kevincrimi@gmail.com>
2017-06-14 15:26:43 -0400

    use heroku's dynamic port

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.get('/guests', function(req, res) {
 	    });
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
